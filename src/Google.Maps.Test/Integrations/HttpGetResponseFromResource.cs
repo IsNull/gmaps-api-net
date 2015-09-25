de17462d6a9ab97799ff04ca2212ce217ed255fb
IsNull <pascalbuettiker@bluewin.ch>
2015-09-25 09:18:29 +0200

    fixed unit tests HttpGetResponseFromResource

--- src/Google.Maps.Test/Integrations/HttpGetResponseFromResource.cs
+++ src/Google.Maps.Test/Integrations/HttpGetResponseFromResource.cs
@@ -4,50 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using System.IO;
+using Google.Maps.Internal;
 
 namespace Google.Maps.Test.Integrations
 {
-	public class HttpGetResponseFromResource : Google.Maps.Internal.Http.HttpGetResponse
+	public class HttpGetResponseFromResource : HttpGetResponse
 	{
 		System.Reflection.Assembly S_testAssembly = Assembly.GetAssembly(typeof(HttpGetResponseFromResource));
 
-		public HttpGetResponseFromResource(Uri uri) : base(uri)
+        public HttpGetResponseFromResource(Uri uri, bool refreshCache)
+            : base(uri, refreshCache)
 		{
 		}
 
 		private string _resourcePath;
 		public string BaseResourcePath { get { return this._resourcePath; } set { this._resourcePath = value; } }
 
-		protected override System.IO.StreamReader GetStreamReader(Uri uri)
-		{
-			string outputType = uri.Segments[uri.Segments.Length - 1];
 
-			System.Text.StringBuilder queryString = new StringBuilder(uri.OriginalString.Substring(uri.OriginalString.IndexOf("?")+1));
-			queryString.Replace("&sensor=false",""); //clear off sensor=false
-			queryString.Replace("&sensor=true", ""); // clear off sensor=true
 
-			//have to replace any remaining ampersands with $ due to filename limitations.
-			queryString.Replace("&", "$").Replace("|","!").Replace("%","~");
 
-			string resourcePath = this.BaseResourcePath + string.Format(".{0}_queries.{1}.{0}", outputType, queryString.ToString());
+	    public override string AsString()
+	    {
+	        var uri = RequestUri;
 
-			Stream resourceStream = S_testAssembly.GetManifestResourceStream(resourcePath);
+	        string outputType = uri.Segments[uri.Segments.Length - 1];
 
-			if (resourceStream == null)
-			{
-				string message = string.Format(
-@"Failed to find resource for query '{0}'.
-BaseResourcePath: '{2}'
-Resource path used: '{1}'
-Ensure a file exists at that resource path and the file has its Build Action set to ""Embedded Resource"".", queryString.ToString(), resourcePath, BaseResourcePath);
-				throw new FileNotFoundException(message);
-			}
+	        var queryString = new StringBuilder(uri.OriginalString.Substring(uri.OriginalString.IndexOf("?") + 1));
+	        queryString.Replace("&sensor=false", ""); //clear off sensor=false
+	        queryString.Replace("&sensor=true", ""); // clear off sensor=true
 
-			return new StreamReader(resourceStream);
-		}
+	        //have to replace any remaining ampersands with $ due to filename limitations.
+	        queryString.Replace("&", "$").Replace("|", "!").Replace("%", "~");
+
+	        string resourcePath = this.BaseResourcePath +
+	                              string.Format(".{0}_queries.{1}.{0}", outputType, queryString.ToString());
+
+	        Stream resourceStream = S_testAssembly.GetManifestResourceStream(resourcePath);
+
+	        if (resourceStream == null)
+	        {
+	            string message = string.Format(
+	                @"Failed to find resource for query '{0}'.
+                    BaseResourcePath: '{2}'
+                    Resource path used: '{1}'
+                    Ensure a file exists at that resource path and the file has its Build Action set to ""Embedded Resource"".",
+	                queryString.ToString(), resourcePath, BaseResourcePath);
+	            throw new FileNotFoundException(message);
+	        }
+	        using (var sr = new StreamReader(resourceStream))
+	        {
+	            return sr.ReadToEnd();
+	        }
+	    }
 	}
 
-	public class HttpGetResponseFromResourceFactory : Google.Maps.Internal.Http.HttpGetResponseFactory
+	public class HttpGetResponseFromResourceFactory : Http.HttpGetResponseFactory
 	{
 		public HttpGetResponseFromResourceFactory(string baseResourcePath)
 		{
@@ -56,9 +67,9 @@ Ensure a file exists at that resource path and the file has its Build Action set
 
 		public string BaseResourcePath { get; set; }
 
-		public override Internal.Http.HttpGetResponse CreateResponse(Uri uri)
+        public override HttpGetResponse CreateResponse(Uri uri, bool forceNoCache = false)
 		{
-			return new HttpGetResponseFromResource(uri) { BaseResourcePath = this.BaseResourcePath };
+            return new HttpGetResponseFromResource(uri, forceNoCache) { BaseResourcePath = this.BaseResourcePath };
 		}
 	}
 }
